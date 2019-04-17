3f8f52cd6e31fa6e30e0b8bee46093ece569a738
MatSol9 <mateusz.soltys@protonmail.com>
2019-04-17 10:17:31 +0200

    Create global.java

---
+++ INPG/labapp_package/global.java
@@ -0,0 +1,12 @@
+package labapp_package;
+
+public final class global {
+	public static final String Super = "Zadania na laboratoria AGH";
+	public static final String Exercises []  = {"Brak æwiczenia" , "Æwiczenie 13" , "Æwiczenie 32", "Æwiczenie 1", "Æwiczenie 51", "Æwiczenie 41"};
+	public static final String Desc_Label = "Wybierz zadanie: ";
+	public static final String Calc_Button = "Oblicz";
+	public static final String Const_Label = "Sta³e:";
+	public static final String Res_Label = "Wyniki:";
+	public static final String Data_Field [] = {"Nieaktywne", "Œrednica kulki [mm]", "Masa kulki [g]", "Czas [s]" , "Rezystancja [\u03A9]", "a [mm]", "k", "k*T", "a_d [mm]", "a_g [mm]"};
+	public static final String Const_Name [] = {"Nieaktywne","g [m/s^2]","Gêstoœæ [kg/m^3]","D³ugoœæ [mm]","Œrednica [mm]", "D³ugoœæ [mm]", "d [mm]", "U(d) [mm]", "Klasa", "Œrednica [mm]", "Niepewnoœæ [mm]"};
+}
