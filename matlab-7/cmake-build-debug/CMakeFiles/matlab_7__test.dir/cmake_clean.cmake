29dc6474d5dc5d42f8d7d6f6c65e52cfe3522f2d
MatSol9 <mateusz.soltys@protonmail.com>
2019-04-17 10:25:50 +0200

    Matlab-7

---
+++ matlab-7/cmake-build-debug/CMakeFiles/matlab_7__test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matlab_7__test.dir/src/matlab.cpp.obj"
+  "CMakeFiles/matlab_7__test.dir/test/test_vector.cpp.obj"
+  "CMakeFiles/matlab_7__test.dir/test/test_matrix.cpp.obj"
+  "CMakeFiles/matlab_7__test.dir/test/main_gtest.cpp.obj"
+  "matlab_7__test.pdb"
+  "matlab_7__test.exe"
+  "matlab_7__test.exe.manifest"
+  "libmatlab_7__test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matlab_7__test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
