3c391197793fa8db39b10af209599333975239d3
MatSol9 <mateusz.soltys@protonmail.com>
2019-04-17 10:17:12 +0200

    Create data_object.java

---
+++ INPG/labapp_package/data_object.java
@@ -0,0 +1,15 @@
+package labapp_package;
+
+public class data_object {
+	String Data_First_String;
+	String Data_Second_String;
+	String Data_Third_String;
+	String Const_First;
+	String Const_Second;
+	String Const_Third;
+	String Const_Fourth;
+	String Exercise_Name;
+	double[] Data_First_Series;
+	double[] Data_Second_Series;
+	double[] Data_Third_Series;
+}
