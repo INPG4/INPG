8d7bd00dc3085117bd86ed43d5c332c4d429690c
MatSol9 <mateusz.soltys@protonmail.com>
2019-04-17 10:19:02 +0200

    Create results.java

---
+++ INPG/labapp_package/results.java
@@ -0,0 +1,12 @@
+package labapp_package;
+
+public class results {
+	String Results_Series;
+	String Results_Average;
+	String Add_Value_1;
+	String Add_Value_2;
+	results(String Results_Average, String Results_Series){
+		this.Results_Average=Results_Average;
+		this.Results_Series=Results_Series;
+	}
+}
